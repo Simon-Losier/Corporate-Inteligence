0b9039d8213ce2bec77bdd254919d1db82c939f5
Simon <98567864+Simon-Losier@users.noreply.github.com>
2024-04-14 17:00:36 -0300

    removed scripts

--- boom_BP/scripts/ID.js
+++
@@ -1,9 +0,0 @@
-import { world } from "@minecraft/server";
-export class onIDScanInteract {
-    constructor() {
-        this.onPlayerInteract = this.onPlayerInteract.bind(this);
-    }
-    onPlayerInteract(eventData) {
-        world.sendMessage("ID Scan");
-    }
-}
